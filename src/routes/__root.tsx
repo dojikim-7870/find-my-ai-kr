04ab803126b2517d4e647858dccbc9c3e560b302
dojikim-7870 <pcdb7777@gmail.com>
2026-08-24 12:47:12 +0900

    Integrate Google Analytics tracking
    Added Google Analytics tracking scripts to the root component.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -117,6 +117,21 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="ko">
       <head>
         <HeadContent />
+        {/* Google Analytics */}
+<script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-HYXRGNPWKE"
+/>
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-HYXRGNPWKE');
+    `,
+  }}
+/>
       </head>
       <body>
         {children}
